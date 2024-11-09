b637ff02f660b109d419d99df64c81940f7a8290
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-11-09 21:24:31 +0100

    feat(http): init golatt

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"embed"
+	"github.com/anhgelus/golatt"
+)
 
+//go:embed templates
+var templates embed.FS
+
+func main() {
+	g := golatt.New(templates)
+	g.DefaultSeoData = &golatt.SeoData{
+		Image:       "",
+		Description: "",
+		Domain:      "now.anhgelus.world",
+	}
+	g.Templates = append(g.Templates, "templates/page/*.gohtml")
+
+	g.StartServer(":80")
 }
