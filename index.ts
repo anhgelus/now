9787e1a1d5364ca056ec033f26e28a76ab6423e1
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-11-14 14:43:09 +0100

    feat(nav): smooth loading while changing page

--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
-const tags = document.querySelectorAll<HTMLElement>(".tag")
-
-tags?.forEach(t => {
-    t.addEventListener("click", _ => {
-        t.classList.toggle("active")
+function setupEvents() {
+    document.querySelectorAll<HTMLElement>(".tag")?.forEach(t => {
+        t.addEventListener("click", _ => {
+            t.classList.toggle("active")
+        })
     })
-})
+
+    document.querySelectorAll<HTMLAnchorElement>("a")?.forEach(a => {
+        a.addEventListener("click", e => {
+            if (!a.href.startsWith(window.location.origin)) return
+            e.preventDefault()
+            changePage(a.href)
+        })
+    })
+}
+
+function changePage(href: string) {
+    fetch(href)
+        .then(resp => resp.text())
+        .then(html => {
+            const doc = new DOMParser().parseFromString(html, "text/html")
+            window.history.pushState({}, "", href)
+            document.title = doc.title
+            const distMain = doc.querySelector("main")
+            const currentMain = document.querySelector("main")
+            if (distMain === null || currentMain === null) document.body = doc.body
+            else currentMain.innerHTML = distMain.innerHTML
+            setupEvents()
+        })
+}
+
+window.addEventListener("popstate", e => window.location.reload())
+setupEvents()
